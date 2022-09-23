d927ede8fd5e6406119d9f1980b745171f1f8334
rhazama <rhazama01@gmail.com>
2022-09-23 10:24:18 -0700

    adjusting helath and attack values

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ## Description
 A Pokemon inspired fighting game where users can battle against the computer until one party's Health Points drop to zero. Users can also upgrade their stats in the shop.
 <br />
-
+<br />
 You can checkout the application through the following link: 
 
 ## Table of Contents
--- assets/js/game.js
+++ assets/js/game.js
@@ -22,7 +22,7 @@ var fightOrSkip = function() {
   // convert promptFight to all lowercase so we can check with less options
   promptFight = promptFight.toLowerCase();
 
-  if (promptFight === "skip")
+  if (promptFight === "skip") {
     // confirm player wants to skip
     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
@@ -38,46 +38,38 @@ var fightOrSkip = function() {
     }
   }
   return false;
-}
+};
 
+// fight function with parameter for enemy's object holding name, health, and attack values
+var fight = function(enemy) {
 //keep track of who goes first
 var isPlayerTurn = true;
+
+// randomly change turn order
 if (Math.random() > .5) {
   isPlayerTurn = false;
 }
-// fight function (now with parameter for enemy's object holding name, health, and attack values)
-var fight = function(enemy) {
-  // keep track of who goes first
-  var isPlayerTurn = true;
 
-  // randomly change turn order
-  if (Math.random() > 0.5) {
-    isPlayerTurn = false;
-  }
+while (playerInfo.health > 0 && enemy.health > 0) {
+  if (isPlayerTurn) {
+    // ask player if they'd like to fight or skip using fightOrSkip function
+    if (fightOrSkip()) {
+      // if true, leave fight by breaking loop
+      break;
+    }
 
-  while (playerInfo.health > 0 && enemy.health > 0) {
-    if (isPlayerTurn) {
-      // ask player if they'd like to fight or skip using fightOrSkip function
-      if (fightOrSkip()) {
-        // if true, leave fight by breaking loop
-        break;
-      }
+    var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
-      var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
-
-      // remove enemy's health by subtracting the amount we set in the damage variable
-      enemy.health = Math.max(0, enemy.health - damage);
-      console.log(
-        playerInfo.name +
-          " attacked " +
-          enemy.name +
-          ". " +
-          enemy.name +
-          " now has " +
-          enemy.health +
-          " health remaining."
-      );
-
+    //remove enemy's health by subtracting the amount we set in the damage variable
+    enemy.health = Math.max(0, enemy.health - damage);
+    console.log(
+      playerInfo.name +
+      " attacked " +
+      enemy.name +
+      " now has " +
+      enemy.health +
+      " health remaining."
+    );
       // check enemy's health
       if (enemy.health <= 0) {
         window.alert(enemy.name + " has died!");
